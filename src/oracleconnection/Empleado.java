e2b45a55b4e855238c3d0423e3fc1f3522581c78
ASUSLOVE\Anwera97 <atch97@gmail.com>
2017-11-15 15:25:50 -0500

    Agregue clase de Empleado y Proyecto

---
+++ src/oracleconnection/Empleado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracleconnection;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Anwera97
+ */
+public class Empleado {
+   private int idEmpleado;
+   private int dni;
+   private int sueldo;
+   private Date fechaContrato;
+   private String correo;
+   private String nombre;
+   
+   
+}
