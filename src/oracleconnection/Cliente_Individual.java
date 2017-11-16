59293a72666666fa93c44db6646c00dfe146510b
ASUSLOVE\Anwera97 <atch97@gmail.com>
2017-11-16 10:47:05 -0500

    Agregue Cliente junto con hijos y modifique Empleado

---
+++ src/oracleconnection/Cliente_Individual.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oracleconnection;
+
+/**
+ *
+ * @author Anwera97
+ */
+public class Cliente_Individual extends Cliente {
+    
+    private int idClienteIndividual;
+}
--- src/oracleconnection/Empleado.java
+++ src/oracleconnection/Empleado.java
@@ -18,6 +18,38 @@ public class Empleado {
    private Date fechaContrato;
    private String correo;
    private String nombre;
-   
+
+    public Empleado(int idEmpleado, int dni, int sueldo, Date fechaContrato, String correo, String nombre) {
+        this.idEmpleado = idEmpleado;
+        this.dni = dni;
+        this.sueldo = sueldo;
+        this.fechaContrato = fechaContrato;
+        this.correo = correo;
+        this.nombre = nombre;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public Date getFechaContrato() {
+        return fechaContrato;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
    
 }
